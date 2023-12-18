a27681661b589a7616b0d2f94ef6ac42a99cb550
Verminas <veremeeva96@gmail.com>
2023-12-18 21:19:38 +0300

    feat: resolve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const sampleActivityNumber = Number(sampleActivity);
+  if (!Number.isFinite(sampleActivityNumber) || typeof sampleActivity !== 'string') return false;
+  if (Number.isFinite(sampleActivityNumber) && sampleActivityNumber > 0 && sampleActivityNumber < MODERN_ACTIVITY) {
+    const age = (Math.log(MODERN_ACTIVITY / sampleActivityNumber) * HALF_LIFE_PERIOD) / Math.LN2;
+    return Math.ceil(age);
+  } else return false;
 }
 
 module.exports = {

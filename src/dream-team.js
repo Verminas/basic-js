ec3cfa073d2af522afe131b90d4f83d43e3cd0ac
Verminas <veremeeva96@gmail.com>
2023-12-18 22:05:01 +0300

    feat: work for createDreamTeam task in process

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+// const arr = members.map((item) => item.trim()).sort();
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let nameDreamTeam = '';
+  if (!Array.isArray(members)) return false;
+  const arr = members.map(function(item) {
+    if (typeof item ==='string') {
+      return item.trim();
+    }
+  }).sort();
+  arr.forEach(function(item) {
+    if (typeof item === 'string' && item !== null) {
+      nameDreamTeam += item[0].toUpperCase();
+    }
+  });
+  return nameDreamTeam;
 }
 
 module.exports = {

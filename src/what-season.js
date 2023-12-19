509572ca283753621caf18effdb28aee236fbfa1
Verminas <veremeeva96@gmail.com>
2023-12-19 18:01:13 +0300

    feat: work for what-season task in process, not pass 4 test

--- src/what-season.js
+++ src/what-season.js
@@ -11,132 +11,144 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
+// function getSeason(date) {
+//   // throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+//
+//   let result;
+//   const year = date.getFullYear();
+//   const month = date.getMonth();
+//   const day = date.getDate();
+//   const seasonsDates = [
+//     {
+//       name: 'winter',
+//       period: [
+//         {
+//           numberMonth: 0,
+//           countOfDays: 31
+//         },
+//         {
+//           numberMonth: 1,
+//           countOfDays: 28,
+//           countOfDaysLeap: 29
+//         },
+//         {
+//           numberMonth: 11,
+//           countOfDays: 31
+//         },
+//       ]
+//     },
+//     {
+//       name: 'spring',
+//       period: [
+//         {
+//           numberMonth: 2,
+//           countOfDays: 31
+//         },
+//         {
+//           numberMonth: 3,
+//           countOfDays: 30
+//         },
+//         {
+//           numberMonth: 4,
+//           countOfDays: 31
+//         },
+//       ]},
+//     {
+//       name: 'summer',
+//       period: [
+//         {
+//           numberMonth: 5,
+//           countOfDays: 30
+//         },
+//         {
+//           numberMonth: 6,
+//           countOfDays: 31},
+//         {
+//           numberMonth: 7,
+//           countOfDays: 31
+//         },
+//       ]},
+//     {
+//       name: 'autumn',
+//       period: [
+//         {
+//           numberMonth: 8,
+//           countOfDays: 30
+//         },
+//         {
+//           numberMonth: 9,
+//           countOfDays: 31
+//         },
+//         {
+//           numberMonth: 10,
+//           countOfDays: 30
+//         },
+//       ]},
+//   ];
+//   let hasErrors = 0;
+//   let leapYear = false;
+//
+//   if (!(date instanceof Date)) {
+//     result = 'Unable to determine the time of year!';
+//     return result;
+//   }
+//
+//   if (year % 4 == 0) leapYear = true;
+//
+//   try {
+//     for (let i = 0; i < seasonsDates.length; i += 1) {
+//       let seasonDate = seasonsDates[i];
+//       for (let j = 0; j < seasonDate.period.length; j += 1) {
+//         if (month == seasonDate.period[j].numberMonth) {
+//           if (!leapYear) {
+//             if (day > 0 && day <= seasonDate.period[j].countOfDays) {
+//               result = seasonDate.name;
+//             } else {
+//               hasErrors += 1;
+//             }
+//           }
+//           if (leapYear && month == seasonDate.period[j].numberMonth) {
+//             if (day > 0 && day <= seasonDate.period[j].countOfDaysLeap) {
+//               result = seasonDate.name;
+//             } else {
+//               hasErrors += 1;
+//             }
+//           }
+//         }
+//       }
+//     }
+//     if (hasErrors > 0) {
+//       result = 'Invalid date!';
+//     }
+//   } catch (e) {
+//     result = e.message;
+//   }
+//
+//   if (!result) {
+//     result = 'Invalid date!';
+//   }
+//
+//   return result;
+// }
 function getSeason(date) {
-  // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-
-  let result;
-  const year = date.getFullYear();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const day = date.getDate();
-  const seasonsDates = [
-    {
-      name: 'winter',
-      period: [
-        {
-          numberMonth: 0,
-          countOfDays: 31
-        },
-        {
-          numberMonth: 1,
-          countOfDays: 28,
-          countOfDaysLeap: 29
-        },
-        {
-          numberMonth: 11,
-          countOfDays: 31
-        },
-      ]
-    },
-    {
-      name: 'spring',
-      period: [
-        {
-          numberMonth: 2,
-          countOfDays: 31
-        },
-        {
-          numberMonth: 3,
-          countOfDays: 30
-        },
-        {
-          numberMonth: 4,
-          countOfDays: 31
-        },
-      ]},
-    {
-      name: 'summer',
-      period: [
-        {
-          numberMonth: 5,
-          countOfDay: 30
-        },
-        {
-          numberMonth: 6,
-          countOfDay: 31},
-        {
-          numberMonth: 7,
-          countOfDay: 31
-        },
-      ]},
-    {
-      name: 'autumn',
-      period: [
-        {
-          numberMonth: 8,
-          countOfDay: 30
-        },
-        {
-          numberMonth: 9,
-          countOfDay: 31
-        },
-        {
-          numberMonth: 10,
-          countOfDay: 30
-        },
-      ]},
-  ];
-  let hasErrors = 0;
-  let leapYear = false;
 
-
-  try {
-    if (date == undefined || date == null) {
-      result = 'Unable to determine the time of year!';
-      return result;
-    }
-    if (typeof date !== 'object' || isNaN(date.getTime())) {
-      hasErrors += 1;
-    }
-    if (hasErrors > 0) {
-      throw new Error('Invalid date!');
-    }
-  } catch(e) {
-    return e.message;
+  if (!(date instanceof Date) || isNaN(date)) {
+    return 'Unable to determine the time of year!';
   }
 
-  if (year % 4 == 0) leapYear = true;
-
-  if (hasErrors == 0) {
-    try {
-      for (let i = 0; i < seasonsDates.length; i += 1) {
-        let seasonDate= seasonsDates[i];
-        for (let j = 0; j < seasonDate.period.length; j += 1) {
-          if (month == seasonDate.period[j].numberMonth) {
-            if (!leapYear) {
-              if (day > 0 && day <= seasonDate.period[j].countOfDays) {
-                result = seasonDate.name;
-              } else {
-                hasErrors += 1;
-              }
-            }
-            if (leapYear && month == seasonsDates[0].period[1].numberMonth) {
-              if (day > 0 && day <= seasonsDates[0].period[1].countOfDaysLeap) {
-                result = seasonsDates[0].name;
-              } else {
-                hasErrors += 1;
-              }
-            }
-          }
-        }
-      }
-      if (hasErrors > 0) {
-        throw new Error('Invalid date!');
-      }
-      return result;
-    } catch(e) {
-      return e.message;
-    }
+  if ((month === 12 && day >= 1) || (month <= 2 && day <= 28)) {
+    return 'winter';
+  } else if (month >= 3 && month <= 5) {
+    return 'spring';
+  } else if (month >= 6 && month <= 8) {
+    return 'summer';
+  } else if (month >= 9 && month <= 11) {
+    return 'autumn';
+  } else {
+    return 'Invalid date!';
   }
 }
 module.exports = {

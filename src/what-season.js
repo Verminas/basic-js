4762a11e865ea7fb71e6c84bc6a41f6bb5e8cdea
Verminas <veremeeva96@gmail.com>
2023-12-19 15:25:16 +0300

    feat: work for what-season task in process

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,100 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result;
+  if (typeof date !== 'object') {
+    result = 'Unable to determine the time of year!';
+    return result;
+  }
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const seasonsDates = [
+    {
+      name: 'winter',
+      period: [
+        {
+          numberMonth: 0,
+          countOfDays: 31
+        },
+        {
+          numberMonth: 1,
+          countOfDays: 28,
+          countOfDaysLeap: 29
+        },
+        {
+          numberMonth: 11,
+          countOfDays: 31
+        },
+      ]
+    },
+    {
+      name: 'spring',
+      period: [
+        {
+          numberMonth: 2,
+          countOfDays: 31
+        },
+        {
+          numberMonth: 3,
+          countOfDays: 30
+        },
+        {
+          numberMonth: 4,
+          countOfDays: 31
+        },
+      ]},
+    {
+      name: 'summer',
+      period: [
+        {
+          numberMonth: 5,
+          countOfDay: 30
+        },
+        {
+          numberMonth: 6,
+          countOfDay: 31},
+        {
+          numberMonth: 7,
+          countOfDay: 31
+        },
+      ]},
+    {
+      name: 'autumn',
+      period: [
+        {
+          numberMonth: 8,
+          countOfDay: 30
+        },
+        {
+          numberMonth: 9,
+          countOfDay: 31
+        },
+        {
+          numberMonth: 10,
+          countOfDay: 30
+        },
+      ]},
+  ];
+  let hasErrors = 0;
+  let leapYear = false;
+
+  try {
+    for (let i = 0; i < seasonsDates.length; i += 1) {
+     let seasonDate= seasonsDates[i];
+      season
+    }
+
+    if (hasErrors > 0) {
+      throw let e = new Error('Invalid date!');
+    }
+    return result;
+  } catch(e) {
+      return e.message;
+  }
 }
 
 module.exports = {

03b6b71e54370f24c77a949a9df0e3ccf4dd3e30
Verminas <veremeeva96@gmail.com>
2023-12-20 00:53:49 +0300

    feat: resolve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,61 +13,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-// function transform(arr) {
-//   // throw new NotImplementedError('Not implemented');
-//   // remove line with error and write your code here
-//
-//     if (!Array.isArray(arr)) {
-//       throw new Error('\'arr\' parameter must be an instance of the Array!');
-//     }
-//     if (arr.length === 0) return arr;
-//     let arrCopy = arr.slice();
-//     const controlSequences = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
-//     function checkNotControlSequences(arrCopy) {
-//       for (let i = 0; i < arrCopy.length; i += 1) {
-//         for (let j = 0; j < controlSequences.length; j += 1) {
-//           if (arrCopy[i] !== controlSequences[j]) {
-//             return true;
-//           }
-//         }
-//       }
-//     }
-//     let checkControlsFirst = checkNotControlSequences(arrCopy);
-//     if (checkControlsFirst) {
-//         return arrCopy;
-//     } else {
-//         for (let i = 0; i < arrCopy.length; i += 1) {
-//             for (let j = 0; j < controlSequences.length; j += 1) {
-//                 if (arrCopy[i] == controlSequences[j]) {
-//                     if (arrCopy[i].includes('discard')) {
-//                         if (arrCopy[i].includes('next') && arrCopy[i + 1] !== undefined) {
-//                             arrCopy.splice(i, 2);
-//                         }
-//                         if (arrCopy[i].includes('next') && arrCopy[i + 1] == undefined) {
-//                             arrCopy.splice(i, 1);
-//                         }
-//                         if (arrCopy[i].includes('prev') && arrCopy[i - 1] !== undefined) {
-//                             arrCopy.splice(i - 1, 2);
-//                         }
-//                         if (arrCopy[i].includes('prev') && arrCopy[i - 1] == undefined) {
-//                             arrCopy.splice(i - 1, 1);
-//                         }
-//                     }
-//
-//                     if (arrCopy[i].includes('double')) {
-//                         if (arrCopy[i].includes('next') && arrCopy[i + 1] !== undefined) {
-//                             arrCopy.splice(i, 1, arrCopy[i + 1]);
-//                         }
-//                         if (arrCopy[i].includes('prev') && arrCopy[i - 1] !== undefined) {
-//                             arrCopy.splice(i, 1, arrCopy[i - 1]);
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         return arrCopy;
-//     }
-// }
 
 function transform(arr) {
     if (!Array.isArray(arr)) {

f937e7fcda2f0fd4e4aa74d33f7707be846d4a40
Verminas <veremeeva96@gmail.com>
2023-12-20 15:34:20 +0300

    feat: work with extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -24,13 +24,84 @@ function repeater(str, options) {
   let newAddition = '';
   let repeatTimes;
   let additionRepeatTimes;
+  let separator = '';
+  let additionSeparator = '';
+
+  let changeSepAddit = '';
+  let changeBigStr = '';
+
+  if (options.length == 0) return str;
 
   if (typeof str !== 'string') {
     newStr = String(str);
+  } else {
+    newStr = str;
   }
-  if (typeof options.addition !== 'string') {
-    newAddition = String(options.addition);
+
+  if (!isFalse(newAddition)) {
+    if (typeof options.addition !== 'string') {
+      newAddition = String(options.addition);
+    } else {
+      newAddition = options.addition;
+    }
+  } else {
+    newAddition = false;
   }
+
+  if (!options.hasOwnProperty(repeatTimes) || options.repeatTimes == false || options.repeatTimes == '0') {
+    repeatTimes = 1;
+  } else {
+    repeatTimes = options.repeatTimes;
+  }
+  if (!options.hasOwnProperty(separator) || options.separator == false) {
+    separator = '+';
+  } else {
+    separator = options.separator;
+  }
+  if (!options.hasOwnProperty(additionRepeatTimes) || options.additionRepeatTimes == false || options.additionRepeatTimes == '0') {
+    additionRepeatTimes = 1;
+  } else {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+  if (!options.hasOwnProperty(additionSeparator) || options.additionSeparator == false) {
+    additionSeparator = '|';
+  } else {
+    additionSeparator = options.additionSeparator;
+  }
+  function repeatStr(string) {
+    return string.repeat(repeatTimes);
+  }
+  function addSeparatorStr(string) {
+    if (repeatTimes > 1) {
+      return separator + string;
+    }
+  }
+  function addAdditional(string) {
+    if (newAddition) {
+      return string + newAddition;
+    }
+  }
+  function repeatAdditional(string) {
+    return string.repeat(additionRepeatTimes);
+  }
+  function addAdditionSeparator(string) {
+    if (additionRepeatTimes > 1) {
+      return additionSeparator + string;
+    }
+  }
+  if (newAddition) {
+    if (newAddition) {
+      result = addAdditional(newStr);
+    }
+    if (additionRepeatTimes > 1) {
+      result
+    }
+  }
+
+
+
+
+
 
 }
 

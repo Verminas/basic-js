87e93b82db6af134f022c5d30c0126519cc757e4
Verminas <veremeeva96@gmail.com>
2023-12-23 23:34:02 +0300

    feat: work with vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  constructor(direct) {
+    this.direct = direct;
+    this.squareVigenere = [];
+    for (let i = 0; i < this.alphabet.length; i++) {
+      let row = this.alphabet.slice(i);
+      row += this.alphabet.slice(0, i);
+      this.squareVigenere.push(row);
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      message = message.toUpperCase();
+      let newKey = '';
+      while (newKey.length < message.length) {
+        for (let i = 0; i < key.length; i++) {
+          newKey += key[i];
+          if ( i === key.length) {
+            i = 0;
+          }
+        }
+      }
+      newKey = newKey.toUpperCase();
+      let encryptMessage = '';
+      let cipherChar = '';
+      for (let i = 0; i < message.length; i++) {
+      let indexCharMessage = this.alphabet.indexOf(i);
+      let indexCharNewKey = this.alphabet.indexOf(i);
+      if (indexCharMessage !== -1) {
+        cipherChar = this.squareVigenere[indexCharMessage][indexCharNewKey];
+      } else {
+        cipherChar = message[i];
+      }
+      encryptMessage += cipherChar;
+      }
+      if (this.direct || this.direct === undefined) {
+        return encryptMessage;
+      } else {
+        let invertedenCryptMessage = '';
+        for (let index = encryptMessage.length - 1; index > -1; index--) {
+          invertedenCryptMessage += encryptMessage[index];
+        }
+        return invertedenCryptMessage;
+      }
+
+    }
+  }
+  decrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
   }
 }
 
